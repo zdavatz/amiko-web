ef2de6307010aa5a668d3aa8490ca5ed77f0673c
b123400 <i@b123400.net>
2024-01-10 00:32:37 +0900

    Basic prescription layout with modals

---
+++ app/assets/javascripts/amiko.prescriptions.js
@@ -0,0 +1,19 @@
+function showDoctorModal() {
+    var modal = document.querySelector('dialog.prescriptions-doctor');
+    modal.showModal();
+}
+
+function closeDoctorModal() {
+    var modal = document.querySelector('dialog.prescriptions-doctor');
+    modal.close();
+}
+
+function showPatientsModal() {
+    var modal = document.querySelector('dialog.prescriptions-address-book');
+    modal.showModal();
+}
+
+function closePatientsModal() {
+    var modal = document.querySelector('dialog.prescriptions-address-book');
+    modal.close();
+}

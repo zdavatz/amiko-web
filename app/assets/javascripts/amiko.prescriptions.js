1c5e3e2a8109e1c5a7ff06d6ad70aaf146958752
b123400 <i@b123400.net>
2024-01-31 16:52:14 +0900

    Read write list patients

--- app/assets/javascripts/amiko.prescriptions.js
+++ app/assets/javascripts/amiko.prescriptions.js
@@ -11,6 +11,7 @@ function closeDoctorModal() {
 
 function showPatientsModal() {
     var modal = document.querySelector('dialog.prescriptions-address-book');
+    listPatients();
     modal.showModal();
 }
 
@@ -96,9 +97,122 @@ function readDoctor() {
     });
 }
 
+var currentPatientId = null;
+function savePatient() {
+    var sexCheckbox = document.querySelector('input[name=address-book-field-sex]:checked');
+    var patient = {
+        surname: document.getElementsByName('address-book-field-surname')[0].value,
+        name: document.getElementsByName('address-book-field-name')[0].value,
+        street: document.getElementsByName('address-book-field-street')[0].value,
+        city: document.getElementsByName('address-book-field-city')[0].value,
+        zip: document.getElementsByName('address-book-field-zip')[0].value,
+        country: document.getElementsByName('address-book-field-country')[0].value,
+        birthday: document.getElementsByName('address-book-field-birthday')[0].value,
+        sex: sexCheckbox ? sexCheckbox.value : "",
+        weight: document.getElementsByName('address-book-field-weight')[0].value,
+        height: document.getElementsByName('address-book-field-height')[0].value,
+        phone: document.getElementsByName('address-book-field-phone')[0].value,
+        email: document.getElementsByName('address-book-field-email')[0].value,
+        bagnumber: document.getElementsByName('address-book-field-bagnumber')[0].value,
+        cardnumber: document.getElementsByName('address-book-field-cardnumber')[0].value,
+        cardexpiry: document.getElementsByName('address-book-field-cardexpiry')[0].value,
+        gln: document.getElementsByName('address-book-field-gln')[0].value,
+    };
+    if (currentPatientId !== null) {
+        patient.id = currentPatientId;
+    }
+    return getPrescriptionDatabase().then(function (db) {
+        return new Promise(function(resolve, reject) {
+            var req = db.transaction("patients", "readwrite")
+                .objectStore("patients")
+                .put(patient);
+            req.onsuccess = function(event) {
+                var patientId = event.target.result;
+                currentPatientId = patientId;
+                resolve(patientId);
+            };
+            req.onerror = reject;
+        });
+    });
+}
+
+function listPatients() {
+    return getPrescriptionDatabase().then(function(db) {
+        return new Promise(function(resolve, reject) {
+            var objectStore = db.transaction("patients").objectStore("patients");
+            var cursor = objectStore.openCursor();
+            var results = [];
+            cursor.onsuccess = function(event) {
+              var cursor = event.target.result;
+              if (cursor) {
+                cursor.continue();
+                results.push(cursor.value);
+              } else {
+                console.log("No more entries!");
+                resolve(results);
+              }
+            };
+            cursor.onerror = reject;
+        });
+    }).then(function(patients) {
+        var container = document.getElementsByClassName('prescriptions-address-book-patients')[0];
+        container.innerHTML = '';
+        patients.forEach(function (patient) {
+            var div = document.createElement('div');
+            div.className = 'prescriptions-address-book-patient';
+            div.innerText = patient.name + ' ' + patient.surname;
+            var id = patient.id;
+            div.onclick = function () {
+                readPatient(id);
+            };
+            container.appendChild(div);
+        });
+    });
+}
+
+function readPatient(id) {
+    return getPrescriptionDatabase().then(function (db) {
+        return new Promise(function(resolve, reject) {
+            var req = db.transaction("patients")
+                .objectStore("patients")
+                .get(id);
+            req.onsuccess = function(event) {
+                resolve(event.target.result);
+            };
+            req.onerror = reject;
+        });
+    }).then(function (patient) {
+        currentPatientId = patient.id;
+        document.getElementsByName('address-book-field-surname')[0].value = patient.surname;
+        document.getElementsByName('address-book-field-name')[0].value = patient.name;
+        document.getElementsByName('address-book-field-street')[0].value = patient.street;
+        document.getElementsByName('address-book-field-city')[0].value = patient.city;
+        document.getElementsByName('address-book-field-zip')[0].value = patient.zip;
+        document.getElementsByName('address-book-field-country')[0].value = patient.country;
+        document.getElementsByName('address-book-field-birthday')[0].value = patient.birthday;
+        document.querySelector('input[name=address-book-field-sex][value=m]').checked =
+        document.querySelector('input[name=address-book-field-sex][value=f]').checked = false;
+        if (patient.sex === 'f' || patient.sex === 'm') {
+            document.querySelector('input[name=address-book-field-sex][value=' + patient.sex + ']').checked = true;
+        }
+        document.getElementsByName('address-book-field-weight')[0].value = patient.weight;
+        document.getElementsByName('address-book-field-height')[0].value = patient.height;
+        document.getElementsByName('address-book-field-phone')[0].value = patient.phone;
+        document.getElementsByName('address-book-field-email')[0].value = patient.email;
+        document.getElementsByName('address-book-field-bagnumber')[0].value = patient.bagnumber;
+        document.getElementsByName('address-book-field-cardnumber')[0].value = patient.cardnumber;
+        document.getElementsByName('address-book-field-cardexpiry')[0].value = patient.cardexpiry;
+        document.getElementsByName('address-book-field-gln')[0].value = patient.gln;
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('doctor-save').addEventListener('click', function() {
         saveDoctor();
         closeDoctorModal();
+    });
+    document.getElementById('patient-save').addEventListener('click', function() {
+        savePatient();
+        listPatients();
     });
 });

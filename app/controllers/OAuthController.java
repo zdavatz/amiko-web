5a5b1bd1f58d5c8b0450e3672ea88832c6a8d02d
b123400 <i@b123400.net>
2024-06-22 15:34:45 +0900

    Put hin client id and secret in config, with proper redirect uri

--- app/controllers/OAuthController.java
+++ app/controllers/OAuthController.java
@@ -6,6 +6,7 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.CompletableFuture;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.typesafe.config.Config;
 import models.ViewContext;
 import play.i18n.Lang;
 import play.i18n.Messages;
@@ -20,39 +21,47 @@ import views.html.index;
 import views.html.oauthcallback;
 
 public class OAuthController extends Controller {
-    static final String HIN_CLIENT_ID = "ch.ywesee";
-    static final String HIN_CLIENT_SECRET = "";
 
     private final WSClient ws;
+    @Inject private Config configuration;
 
     @Inject
     public OAuthController(WSClient ws) {
         this.ws = ws;
     }
 
-    public String authUrlWithApplicationName(String applicationName) {
-        return "https://apps.hin.ch/REST/v1/OAuth/GetAuthCode/" + applicationName + "?response_type=code&client_id=" + HIN_CLIENT_ID + "&redirect_uri=" + this.redirectUri() + "&state=" + applicationName;
+    public String authUrlWithApplicationName(Http.Request request, String applicationName) {
+        return "https://apps.hin.ch/REST/v1/OAuth/GetAuthCode/" + applicationName + "?response_type=code&client_id=" + getClientId() + "&redirect_uri=" + this.redirectUri(request) + "&state=" + applicationName;
     }
 
-    public String redirectUri() {
-        return "amiko://oauth";
+    public String redirectUri(Http.Request request) {
+        String host = request.host();
+        return "https://" + host + "/oauth/callback";
+    }
+
+    public String getClientId() {
+        return configuration.getString("feature.hin_client_id");
+    }
+
+    public String getClientSecret() {
+        return configuration.getString("feature.hin_client_secret");
     }
 
     public Result sdsAuth(Http.Request request) {
-        return redirect(authUrlWithApplicationName("hin_sds"));
+        return redirect(authUrlWithApplicationName(request, "hin_sds"));
         // ADSwiss_CI-Test
         // ADSwiss_CI
     }
     public Result adswissAuth(Http.Request request) {
         // ADSwiss_CI-Test
         // ADSwiss_CI
-        return redirect(authUrlWithApplicationName("ADSwiss_CI-Test"));
+        return redirect(authUrlWithApplicationName(request, "ADSwiss_CI-Test"));
     }
 
     public CompletionStage<Result> oauthCallback(Http.Request request, String code, String state) {
         String subdomainLang = request.transientLang().orElse(Lang.forCode("de")).language();
         String redirectDest  = subdomainLang == "de" ? "/rezept" : "/prescription";
-        return fetchAccessTokenFromOAuthCode(code)
+        return fetchAccessTokenFromOAuthCode(request, code)
         .thenApply((String accessTokenRes) -> {
             return ok(oauthcallback.render(redirectDest, accessTokenRes));
         });
@@ -64,18 +73,17 @@ public class OAuthController extends Controller {
         return ok(index.render("", "", "", "", "", ctx, null));
     }
 
-    public CompletionStage<String> fetchAccessTokenFromOAuthCode(String code) {
-        CompletionStage<WSResponse> request = ws.url("https://oauth2.hin.ch/REST/v1/OAuth/GetAccessToken")
+    public CompletionStage<String> fetchAccessTokenFromOAuthCode(Http.Request request, String code) {
+        return ws.url("https://oauth2.hin.ch/REST/v1/OAuth/GetAccessToken")
             .addHeader("Accept", "application/json")
             .addHeader("Content-Type", "application/x-www-form-urlencoded")
             .post(
                 "grant_type=authorization_code"
-                + "&redirect_uri=" + URLEncoder.encode(this.redirectUri(), StandardCharsets.UTF_8)
+                + "&redirect_uri=" + URLEncoder.encode(this.redirectUri(request), StandardCharsets.UTF_8)
                 + "&code=" + URLEncoder.encode(code, StandardCharsets.UTF_8)
-                + "&client_id=" + HIN_CLIENT_ID
-                + "&client_secret=" + HIN_CLIENT_SECRET
-            );
-        return request.thenApply(
+                + "&client_id=" + getClientId()
+                + "&client_secret=" + getClientSecret()
+            ).thenApply(
           (WSResponse r) -> {
               return r.getBody();
           });
@@ -88,10 +96,10 @@ public class OAuthController extends Controller {
         }
         String refreshToken = tokenParams[0];
         String postBody = "grant_type=refresh_token"
-                    + "&redirect_uri=" + URLEncoder.encode(this.redirectUri(), StandardCharsets.UTF_8)
+                    + "&redirect_uri=" + URLEncoder.encode(this.redirectUri(request), StandardCharsets.UTF_8)
                     + "&refresh_token=" + URLEncoder.encode(refreshToken, StandardCharsets.UTF_8)
-                    + "&client_id=" + HIN_CLIENT_ID
-                    + "&client_secret=" + HIN_CLIENT_SECRET;
+                    + "&client_id=" + getClientId()
+                    + "&client_secret=" + getClientSecret();
         System.out.println("postBody: " + postBody);
         return ws.url("https://oauth2.hin.ch/REST/v1/OAuth/GetAccessToken")
                 .addHeader("Accept", "application/json")
